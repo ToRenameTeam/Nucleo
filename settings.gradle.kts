402feaea433cfab114de3bde10f723e4f13799b8
LucaSamore <luca.samore.01@gmail.com>
2026-01-26 18:24:25 +0100

    chore: add toolchains resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 rootProject.name = "Nucleo"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
