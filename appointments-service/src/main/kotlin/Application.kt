48775c4dfef904c1467bc603db3b9e80c89fc269
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2026-01-28 00:19:56 +0100

    chore(appointments): add ktor dependency

---
+++ appointments-service/src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package it.nucleo
+
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+            routing { get("/") { call.respondText("Hello sweetie 😍!") } }
+        }
+        .start(wait = true)
+}
