93f406501a0baf046963a3364ee16716729f4bad
LucaSamore <luca.samore.01@gmail.com>
2026-02-28 15:44:11 +0100

    refactor(build-logic): custom plugin for ktor projects

---
+++ build-logic/src/main/kotlin/ktor.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("kotlin-base")
+}
+
+val catalog: VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+dependencies {
+    implementation(catalog.findLibrary("ktor-server-core").get())
+    implementation(catalog.findLibrary("ktor-server-netty").get())
+    implementation(catalog.findLibrary("ktor-server-content-negotiation").get())
+    implementation(catalog.findLibrary("ktor-serialization-kotlinx-json").get())
+    implementation(catalog.findLibrary("ktor-server-status-pages").get())
+    implementation(catalog.findLibrary("ktor-server-cors").get())
+    implementation(catalog.findLibrary("logback").get())
+
+    testImplementation(catalog.findLibrary("ktor-server-test-host").get())
+}
