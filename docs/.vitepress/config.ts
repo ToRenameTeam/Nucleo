b3064838da445c8dab2c591c4492306d5c036767
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2026-03-05 21:32:39 +0100

    docs: add base to VitePress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/Nucleo/',
   title: "Nucleo",
   description: "Nucleo's documentation",
   themeConfig: {
