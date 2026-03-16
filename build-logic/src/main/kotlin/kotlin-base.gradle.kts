d1c014b6e1a3eb771e84031a7f9c516d4a5260a8
LucaSamore <luca.samore.01@gmail.com>
2026-03-16 15:26:00 +0100

    ci: use separate task for running integration tests in kt services

--- build-logic/src/main/kotlin/kotlin-base.gradle.kts
+++ build-logic/src/main/kotlin/kotlin-base.gradle.kts
@@ -1,4 +1,6 @@
 import dev.detekt.gradle.extensions.DetektExtension
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.tasks.testing.Test
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
@@ -19,6 +21,25 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+val testSourceSet = extensions.getByType<JavaPluginExtension>().sourceSets.getByName("test")
+
+val integrationTest by
+    tasks.registering(Test::class) {
+        description = "Runs integration tests only."
+        group = LifecycleBasePlugin.VERIFICATION_GROUP
+
+        testClassesDirs = testSourceSet.output.classesDirs
+        classpath = testSourceSet.runtimeClasspath
+        shouldRunAfter(tasks.test)
+        useJUnitPlatform()
+
+        filter {
+            includeTestsMatching("it.nucleo.*.integration.*")
+            includeTestsMatching("*IntegrationTest")
+            isFailOnNoMatchingTests = false
+        }
+    }
 
 val javaVersion: String by project
 
